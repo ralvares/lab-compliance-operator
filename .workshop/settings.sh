39d5e9e2727487728a05ea6f3d3ca6fcc8314c18
Rodrigo Alvares <ralvares@redhat.com>
2020-10-15 04:38:47 +0000

    Add scripts to build workshop

---
+++ .workshop/settings.sh
@@ -0,0 +1,12 @@
+WORKSHOP_NAME=lab-compliance-operator
+WORKSHOP_IMAGE=quay.io/ralvares/lab-compliance-operator:latest
+WORKSHOP_TITLE="OCP Compliance Operator"
+WORKSHOP_DESCRIPTION="Get started with compliance Operator"
+RESOURCE_BUDGET=medium
+MAX_SESSION_AGE=36000
+IDLE_TIMEOUT=36000
+SPAWNER_MODE=hosted-workshop
+
+SPAWNER_MODE=learning-portal
+SPAWNER_VARIANT=production
+DASHBOARD_VARIANT=production
